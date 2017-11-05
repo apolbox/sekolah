c28c1974237e1f82c12725f1ff09b9410ee5f984
Ayus Irfang Filaras <ayus.sahabat@gmail.com>
2017-11-05 07:54:42 +0700

    Update index.php

--- src/Sekolah/index.php
+++ src/Sekolah/index.php
@@ -1,37 +1,19 @@
-<!DOCTYPE html>
-<html>
-<head>
-<link rel="stylesheet" type="text/css" href="css/style.css">
-	<title>Sistem Informasi Akademik</title>
-</head>
-<body>
-<div class="header">
-	<div class="kanan">
-		<ul>
-			<li><a href="#signupindex"><font color="white">Buat Akun</font></a></li>
-			<li><a href="login.php"><font color="white">Masuk</font></a></li>
-	</div>
-</div>
+<?php
+/**
+ * File kerja - Utama
+ *
+ * @since version 1.0.0
+ * @author ayus irfang filaras <ayus.sahabat@gmail.com>
+ */
+namespace SRC;
 
-<div class="landingpage"><br><br>
-	<div class="landingpagekiri"><h1>Tanpa Ribet</h1>
-		<h4>Mudah digunakan. Sebagai guru, Anda bisa menambah data siswa<br>
-		walaupun Anda belum pernah menggunakan situs ini.<br>
-		Anda dengan mudahnya mengelola data,<br>
-		 semudah mengorek upil.</h4>
-	</div>
+// Memanggil fungsi
+require __DIR__.'/library/funcX.php';
 
-	<div class="landingpagekanan"><h1>Terjaga Keamanannya</h1>
-		<h4>Data Anda sebagai siswa aman. Yang bisa melihat data Anda hanyalah Anda sendiri.
-		Teman Anda pun tidak bisa melihat data-data Anda, termasuk nilai. Cukup Menakjubkan !
-		Dengan begitu, Anda tidak perlu berkecil hati apabila nilai Anda jelek.</h4>
-	</div>
-</div>
-
-<div id="signupindex">
-	<br><center class="signuptitle"><h2>Tertarik Untuk Mendaftar ? Gratis !</h2>
-	<hr noshade width="90%" height="1"><br>
-	<a href="signup.php"><input type="button" value="Ya, Saya Tertarik" class="signupbtn"></a></center>
-</div>
-</body>
-</html>
+class Utama
+{
+	public function __construct()
+	{
+		setContentview('welcome');
+	}
+}
