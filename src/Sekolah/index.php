23ff4576163917f50f30dcc84b42943ccd205f36
Ayus Irfang Filaras <ayus.sahabat@gmail.com>
2017-11-05 07:55:19 +0700

    Update index.php

--- src/Sekolah/index.php
+++ src/Sekolah/index.php
@@ -5,7 +5,7 @@
  * @since version 1.0.0
  * @author ayus irfang filaras <ayus.sahabat@gmail.com>
  */
-namespace SRC;
+namespace SRC\Sekolah;
 
 // Memanggil fungsi
 require __DIR__.'/library/funcX.php';
